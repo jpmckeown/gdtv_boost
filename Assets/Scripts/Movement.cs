15261a5521bc18f259bd0a93eb720b6c7b2f3d02
jpmckeown <j.p.mckeown@icloud.com>
2023-07-15 15:45:31 +0100

    force on rocket

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -16,12 +17,13 @@ public class Movement : MonoBehaviour
 
     void ProcessInput(){
         if(Input.GetKey(KeyCode.Space)) {
+            rb.AddRelativeForce(0,1,0);
             Debug.Log("Space key pressed for rocket motor.");
         }
         if(Input.GetKey(KeyCode.A)){
             Debug.Log("Left arrow key pressed");
         }
-        if(Input.GetKey(KeyCode.D)){
+        else if(Input.GetKey(KeyCode.D)){
             Debug.Log("Right arrow key pressed");
         }
     }
