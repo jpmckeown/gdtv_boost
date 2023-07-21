6a37c9a847782a94903ceb2c6821adcac1fea081
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2023-07-21 16:54:46 +0100

    oscillating obstacle movalbe by manual slider

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -5,6 +5,7 @@ public class Oscillator : MonoBehaviour
     Vector3 startPosition;
     public Vector3 moveVector;
     [SerializeField] [Range(0,1)] float moveFactor;
+    // [SerializeField] float period = 2f;
 
     void Start()
     {
